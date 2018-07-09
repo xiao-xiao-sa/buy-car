abff9a0f8bb10877988f79438301e0887acce696
yezi <1847214529@qq.com>
2018-07-09 09:25:19 +0800

    2018/7/7

---
+++ src/assets/data/consumeData.js
@@ -0,0 +1,12 @@
+var consumeList = [
+	{"id":"01","time":1234567895,"model":"奥迪-A4Lsdfa","count":1,"ktConsume":10000,"ctConsume":0},
+	{"id":"01","time":1234567995,"model":"奥迪-A4L","count":1,"ktConsume":10000,"ctConsume":0},
+	{"id":"01","time":1234587895,"model":"奥迪-A4L","count":1,"ktConsume":0,"ctConsume":10000},
+	{"id":"01","time":1234567895,"model":"奥迪-A4L","count":1,"ktConsume":0,"ctConsume":10000},
+	{"id":"01","time":1254567895,"model":"奥迪-A4L","count":1,"ktConsume":0,"ctConsume":10000},
+	{"id":"01","time":1334567895,"model":"奥迪-A4L","count":1,"ktConsume":10000,"ctConsume":0}
+]
+
+module.exports = {
+	consumeList:consumeList
+}
---
+++ src/assets/data/myIntegrationData.js
@@ -0,0 +1,17 @@
+var myIntegrationList = [
+	{"type":"消费",'time':1234567891,"num":10000},
+	{"type":"消费",'time':1234567891,"num":10000},
+	{"type":"消费",'time':1234567891,"num":10000},
+	{"type":"提现",'time':1234567891,"num":10000},
+	{"type":"提现",'time':1234567891,"num":10000},
+	{"type":"提现",'time':1234567891,"num":10000},
+	{"type":"获取",'time':1234567891,"num":10000},
+	{"type":"获取",'time':1234567891,"num":10000},
+	{"type":"获取",'time':1234567891,"num":10000},
+	{"type":"获取",'time':1234567891,"num":10000},
+	{"type":"获取",'time':1234567891,"num":10000}
+]
+
+module.exports = {
+	myIntegrationList:myIntegrationList
+}
